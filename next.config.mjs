83dbd9b44fd438541564206598be5761b8bfe9c5
glafver <glafver22@gmail.com>
2024-09-16 20:31:41 +0200

    fix suspense

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['storage.cloud.google.com', 'storage.googleapis.com'],
     },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
 };
 
 export default nextConfig; 
