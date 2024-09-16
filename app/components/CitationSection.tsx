8d05a98fdac2a7a610133e2004d9d831339ea7db
glafver <glafver22@gmail.com>
2024-09-16 20:15:33 +0200

    eslint fix

--- app/components/CitationSection.tsx
+++ app/components/CitationSection.tsx
@@ -5,7 +5,7 @@ const CitationSection: React.FC = () => {
         <section className="py-16 flex justify-center items-center">
             <div className="min-w-fit mx-auto px-4 text-center">
                 <blockquote className="text-xl lg:text-2xl italic max-w-2xl mx-auto">
-                    "Photography captures the character of a home, turning everyday moments into lasting memories. <br /> I help make those moments stand out."
+                    &quot;Photography captures the character of a home, turning everyday moments into lasting memories. <br /> I help make those moments stand out.&quot;
                 </blockquote>
                 <p className="mt-4 text-end text-lg "> Glafira Veretennikova, photographer</p>
             </div>
